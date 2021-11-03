0421a217118bf341771f1f9037827131a258c9f0
이무원 <whitewind@imuwon-ui-iMac.local>
2021-11-03 21:37:48 +0900

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get('/', (req, res)=>{
   res.send('this is MuSu Server');
