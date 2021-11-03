d458f9ec55f21117ff28cdb66aabd8ab4773e646
이무원 <whitewind@imuwon-ui-iMac.local>
2021-11-03 22:46:33 +0900

    errorhandle

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 const port = process.env.PORT || 3001;
-const clientDomain = process.env.CLIENT_ORIGIN || 'http://localhost:3001';
+const clientDomain = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 
 const httpServer = createServer(app);
 const io = require("socket.io")(httpServer, {
