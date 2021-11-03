dd95fb288b12ed746f56265ed6ea703403225273
이무원 <whitewind@imuwon-ui-iMac.local>
2021-11-03 21:51:35 +0900

    socket

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
+const httpServer = createServer(app);
 const port = process.env.PORT || 3001;
 
 app.use(cors());
+
+
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log(socket);
+
+});
+
 
 app.get('/', (req, res)=>{
   res.send('this is MuSu Server');
 });
 
-app.listen(port);
+httpServer.listen(port);
